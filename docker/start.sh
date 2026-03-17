4418e9607813ed82148fdf17edfca09b8e6a0ea0
Yasminingrum <tyasminingrum@student.ciputra.ac.id>
2026-03-17 15:12:34 +0700

    fix nginx startup

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+set -e
+
+# Start php-fpm in background
 php-fpm -D
+
+# Wait for php-fpm to be ready
 sleep 2
-nginx -g 'daemon off;'
+
+# Test nginx config
+nginx -t
+
+# Start nginx
+exec nginx -g 'daemon off;'
